3386de9955fe1d4a03e9a358e2a5186998fa9fd8
indoos@gmail.com <indoos@gmail.com@969d9cd8-0433-72b4-3c02-a48c5f1c977b>
2011-01-25 19:25:23 +0000

    branching for cassandra 0.7

---
+++ branches/cassui-cassandra-0.6.x/grails-app/domain/com/impetus/ilabs/cassui/Server.groovy
@@ -0,0 +1,37 @@
+package com.impetus.ilabs.cassui
+
+class Server {
+	
+	String cassandraURL
+	String cassandraVersion = "0.6.4"
+
+	def getServer(){
+		if (cassandraURL) {
+			if (cassandraURL.indexOf(":")!=-1){
+				return cassandraURL.substring(0,cassandraURL.indexOf(":"))
+			} else {
+				return cassandraURL
+			}
+		} 
+		return "localhost"
+	}
+	def getPort(){
+		if (cassandraURL) {
+			if (cassandraURL.indexOf(":")!=-1){
+				int port
+				try{
+					port = Integer.parseInt(cassandraURL.substring(cassandraURL.indexOf(":")+1))
+				} catch (Exception e){
+					port=9160
+				}
+				return port
+			}
+		}
+		return 9160
+	}
+
+    static constraints = {
+		cassandraURL()
+		cassandraVersion(inList:["0.6.4","0.7"])
+    }
+}
